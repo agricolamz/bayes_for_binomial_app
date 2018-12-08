117b67d6bcf9e4e486d1a57d9a909e0806047bd4
George Moroz <agricolamz@gmail.com>
2018-12-08 19:21:52 +0300

    increase max value

--- ui.R
+++ ui.R
@@ -6,10 +6,10 @@ shinyUI(fluidPage(
     
     sidebarLayout(
         sidebarPanel(
-                    sliderInput('data_alpha', 'data shape 1:', min=1, max=50, value=6, step=1),
-                    sliderInput('data_beta', 'data shape 2:',  min=1, max=50, value=21, step=1),
-                    sliderInput('prior_alpha', 'prior shape 1:', min=0.1, max=50, value=10, step=1),
-                    sliderInput('prior_beta', 'prior shape 2:',  min=0.1, max=50, value=10, step=1)
+                    sliderInput('data_alpha', 'data shape 1:', min=1, max=100, value=6, step=1),
+                    sliderInput('data_beta', 'data shape 2:',  min=1, max=100, value=21, step=1),
+                    sliderInput('prior_alpha', 'prior shape 1:', min=0.1, max=100, value=10, step=1),
+                    sliderInput('prior_beta', 'prior shape 2:',  min=0.1, max=100, value=10, step=1)
             ),
         mainPanel(
             textOutput('header'),
